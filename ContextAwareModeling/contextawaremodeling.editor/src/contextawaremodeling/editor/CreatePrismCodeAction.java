394deb09de9d57c4d56ce114881ffba74b7e9024
cproinger <christian.proinger@gmail.com>
2016-05-26 22:59:33 +0200

    generating the file through the eclipse api

--- ContextAwareModeling/contextawaremodeling.editor/src/contextawaremodeling/editor/CreatePrismCodeAction.java
+++ ContextAwareModeling/contextawaremodeling.editor/src/contextawaremodeling/editor/CreatePrismCodeAction.java
@@ -1,34 +1,46 @@
 package contextawaremodeling.editor;
 
+import java.io.IOException;
+
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.statushandlers.StatusManager;
 
 public class CreatePrismCodeAction implements IObjectActionDelegate {
 
+	private static final String PLUGIN_ID = "contextawaremodeling.editor";
+	
 	private ISelection currentSelection;
+	
+	@SuppressWarnings("unused")
 	private Shell shell;
 
 	@Override
 	public void run(IAction action) {
 		IStructuredSelection iss = (IStructuredSelection)currentSelection;
+		
 		int i = 0;
 		IFile sourceFile = (IFile) iss.getFirstElement();
-		IPath targetFile = sourceFile.getFullPath().removeFileExtension().addFileExtension("prism");
-//		for (Iterator<?> iterator = iss.iterator(); iterator.hasNext();) {
-//			file = (IFile)iterator.next();
-//			managers[i] = file.getFullPath().toString();
-//			managers_name[i] = file.getName().toString().replace(".mngr", "");
-//			i++;
-//		}
-//		outctxmngr = file.getProject().getFullPath().toString()
-//				     +"/"+managers_name[0]+"-"+managers_name[1]+".ctxmngr";
-		new Hom2Prism(sourceFile, targetFile).generateCode();
+//		IPath targetFile = sourceFile.getLocation().removeFileExtension().addFileExtension("prism");
+		IPath targetFile = sourceFile.getProjectRelativePath().removeFileExtension().addFileExtension("prism");
+		IFile targetProjectFile = sourceFile.getProject().getFile(targetFile);
+		
+		try {
+			new Hom2Prism(sourceFile, targetProjectFile).generateCode();
+		} catch (IOException | CoreException e) {
+			IStatus status = new Status(IStatus.ERROR, PLUGIN_ID, e.getMessage(), e);
+			StatusManager.getManager().handle(status, StatusManager.SHOW);
+			e.printStackTrace();
+		}
 	}
 
 	@Override
--- ContextAwareModeling/contextawaremodeling.editor/src/contextawaremodeling/editor/Hom2Prism.java
+++ ContextAwareModeling/contextawaremodeling.editor/src/contextawaremodeling/editor/Hom2Prism.java
@@ -1,13 +1,21 @@
 package contextawaremodeling.editor;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.text.DecimalFormat;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
 import java.util.Locale;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
@@ -25,15 +33,15 @@ import mm.ManagerModel.Transition;
 
 public class Hom2Prism {
 
-	private IPath targetFile;
+	private IFile targetFile;
 	private IFile sourceFile;
 
-	public Hom2Prism(IFile sourceFile, IPath targetFile) {
+	public Hom2Prism(IFile sourceFile, IFile targetFile) {
 		this.sourceFile = sourceFile;
 		this.targetFile = targetFile;
 	}
 
-	public void generateCode() {
+	public void generateCode() throws FileNotFoundException, IOException, CoreException {
 		ResourceSet resourceSet = new ResourceSetImpl();
 
 		// Register the appropriate resource factory to handle all file
@@ -46,87 +54,55 @@ public class Hom2Prism {
 		//
 		resourceSet.getPackageRegistry().put(FOMCompositionModelPackage.eNS_URI, FOMCompositionModelPackage.eINSTANCE);
 
-		// If there are no arguments, emit an appropriate usage message.
+		URI uri = URI.createFileURI(sourceFile.getLocation().toFile().getAbsolutePath());
+		// Demand load resource for this file.
 		//
-		// if (args.length == 0) {
-		// System.out.println("Enter a list of file paths or URIs that have
-		// content like this:");
-		// try {
-		// Resource resource =
-		// resourceSet.createResource(URI.createURI("http:///My.fomc"));
-		// ContextManager root =
-		// FOMCompositionModelFactory.eINSTANCE.createContextManager();
-		// resource.getContents().add(root);
-		// resource.save(System.out, null);
-		// } catch (IOException exception) {
-		// exception.printStackTrace();
-		// }
-		// } else {
+		Resource resource = resourceSet.getResource(uri, true);
 
-		// Iterate over all the arguments.
+		// Validate the contents of the loaded resource.
 		//
-		// for (int i = 0; i < args.length; ++i) {
-		// Construct the URI for the instance file.
-		// The argument is treated as a file path only if it denotes an
-		// existing file.
-		// Otherwise, it's directly treated as a URL.
-		//
-		// File file = new File(args[i]);
-
-		try {
-			URI uri = URI.createFileURI(sourceFile.getLocation().toFile().getAbsolutePath());
-			// Demand load resource for this file.
-			//
-			Resource resource = resourceSet.getResource(uri, true);
-			System.out.println("Loaded " + uri);
-
-			// Validate the contents of the loaded resource.
-			//
-			for (EObject eObject : resource.getContents()) {
-				Diagnostic diagnostic = Diagnostician.INSTANCE.validate(eObject);
-				if (diagnostic.getSeverity() != Diagnostic.OK) {
-					//TODO only transform valid models. 
-					printDiagnostic(diagnostic, "");
-				}
-				if(eObject instanceof Manager) {
-					System.out.println("we got a manager!");
-					generatePrismCode((Manager) eObject);
-				}
+		for (EObject eObject : resource.getContents()) {
+			Diagnostic diagnostic = Diagnostician.INSTANCE.validate(eObject);
+			if (diagnostic.getSeverity() != Diagnostic.OK) {
+				// TODO only transform valid models.
+				printDiagnostic(diagnostic, "");
 			}
-		} catch (RuntimeException exception) {
-//			System.out.println("Problem loading " + uri);
-			exception.printStackTrace();
+			if (eObject instanceof Manager) {
+				generatePrismCode((Manager) eObject);
+			}
 		}
-	
 
-	// }
 	}
 
-	private void generatePrismCode(Manager manager) {
-		PrintStream os = System.out;
-		
-		os.println("ctmc");
-		os.println();
-		os.println("module hom");
-		EList<State> states = manager.getStates();
-		os.println(MessageFormat.format("\ths : [0..{0}] init {1};", (states.size()-1), states.indexOf(manager.getStart())));
-		for(int i = 0; i < states.size(); i++) {
-			os.println("//" + i + "=" + states.get(i).getName());
-		}
-		DecimalFormat f = (DecimalFormat)NumberFormat.getNumberInstance(Locale.ENGLISH);
-		f.applyPattern("#.##########################");
-		for(int i = 0; i < states.size(); i++) {
-			State s = states.get(i);
-			for(Transition t : s .getTransitions()) {
-				os.println(MessageFormat.format("\t[t_{0}_2_{1}] hs={2} -> {3} : (hs''={4});", 
-						toPrismName(s), 
-						toPrismName(t.getTargetState()), 
-						i, 
-						f.format(t.getRate()), 
-						states.indexOf(t.getTargetState())));
+	private void generatePrismCode(Manager manager) throws FileNotFoundException, IOException, CoreException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try (PrintStream ps = new PrintStream(baos);) {
+
+			ps.println("ctmc");
+			ps.println();
+			ps.println("module hom");
+			EList<State> states = manager.getStates();
+			ps.println(MessageFormat.format("\ths : [0..{0}] init {1};", (states.size() - 1),
+					states.indexOf(manager.getStart())));
+			for (int i = 0; i < states.size(); i++) {
+				ps.println("//" + i + "=" + states.get(i).getName());
 			}
+			DecimalFormat f = (DecimalFormat) NumberFormat.getNumberInstance(Locale.ENGLISH);
+			f.applyPattern("#.##########################");
+			for (int i = 0; i < states.size(); i++) {
+				State s = states.get(i);
+				for (Transition t : s.getTransitions()) {
+					ps.println(MessageFormat.format("\t[t_{0}_2_{1}] hs={2} -> {3} : (hs''={4});", toPrismName(s),
+							toPrismName(t.getTargetState()), i, f.format(t.getRate()),
+							states.indexOf(t.getTargetState())));
+				}
+			}
+			ps.println("endmodule");
 		}
-		os.println("endmodule");
+		targetFile.create(new ByteArrayInputStream(baos.toByteArray()), 
+				true, 
+				new NullProgressMonitor());
+		
 	}
 
 	private String toPrismName(State s) {
