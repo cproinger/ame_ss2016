84187ec867f21bee59f6785455107f83a08d221e
cproinger <christian.proinger@gmail.com>
2016-05-27 10:55:29 +0200

    java code hom generation started (only states by now)

--- ContextAwareModeling/contextawaremodeling.editor/src/contextawaremodeling/editor/CreatePrismCodeAction.java
+++ ContextAwareModeling/contextawaremodeling.editor/src/contextawaremodeling/editor/CreatePrismCodeAction.java
@@ -8,49 +8,26 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.statushandlers.StatusManager;
 
-public class CreatePrismCodeAction implements IObjectActionDelegate {
-
-	private static final String PLUGIN_ID = "contextawaremodeling.editor";
-	
-	private ISelection currentSelection;
-	
-	@SuppressWarnings("unused")
-	private Shell shell;
+public class CreatePrismCodeAction extends AbstractCAMAction {
 
 	@Override
 	public void run(IAction action) {
 		IStructuredSelection iss = (IStructuredSelection)currentSelection;
 		
-		int i = 0;
 		IFile sourceFile = (IFile) iss.getFirstElement();
-//		IPath targetFile = sourceFile.getLocation().removeFileExtension().addFileExtension("prism");
 		IPath targetFile = sourceFile.getProjectRelativePath().removeFileExtension().addFileExtension("prism");
 		IFile targetProjectFile = sourceFile.getProject().getFile(targetFile);
 		
 		try {
-			new Hom2Prism(sourceFile, targetProjectFile).generateCode();
+			new Hom2Prism(sourceFile, targetProjectFile).generate();
 		} catch (IOException | CoreException e) {
 			IStatus status = new Status(IStatus.ERROR, PLUGIN_ID, e.getMessage(), e);
 			StatusManager.getManager().handle(status, StatusManager.SHOW);
 			e.printStackTrace();
 		}
-	}
-
-	@Override
-	public void selectionChanged(IAction action, ISelection selection) {
-		this.currentSelection = selection;
-	}
-
-	@Override
-	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		shell = targetPart.getSite().getShell();
 	}
 
 }
--- ContextAwareModeling/contextawaremodeling.editor/src/contextawaremodeling/editor/Hom2Prism.java
+++ ContextAwareModeling/contextawaremodeling.editor/src/contextawaremodeling/editor/Hom2Prism.java
@@ -3,10 +3,8 @@ package contextawaremodeling.editor;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.text.DecimalFormat;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
@@ -14,64 +12,24 @@ import java.util.Locale;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.util.Diagnostician;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
-import fcm.FOMCompositionModel.FOMCompositionModelPackage;
 import mm.ManagerModel.Manager;
 import mm.ManagerModel.State;
 import mm.ManagerModel.Transition;
 
-public class Hom2Prism {
-
-	private IFile targetFile;
-	private IFile sourceFile;
+public class Hom2Prism extends AbstractFileGenerator {
 
 	public Hom2Prism(IFile sourceFile, IFile targetFile) {
-		this.sourceFile = sourceFile;
-		this.targetFile = targetFile;
+		super(sourceFile, targetFile);
 	}
 
-	public void generateCode() throws FileNotFoundException, IOException, CoreException {
-		ResourceSet resourceSet = new ResourceSetImpl();
-
-		// Register the appropriate resource factory to handle all file
-		// extensions.
-		//
-		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
-				.put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
-
-		// Register the package to ensure it is available during loading.
-		//
-		resourceSet.getPackageRegistry().put(FOMCompositionModelPackage.eNS_URI, FOMCompositionModelPackage.eINSTANCE);
-
-		URI uri = URI.createFileURI(sourceFile.getLocation().toFile().getAbsolutePath());
-		// Demand load resource for this file.
-		//
-		Resource resource = resourceSet.getResource(uri, true);
-
-		// Validate the contents of the loaded resource.
-		//
-		for (EObject eObject : resource.getContents()) {
-			Diagnostic diagnostic = Diagnostician.INSTANCE.validate(eObject);
-			if (diagnostic.getSeverity() != Diagnostic.OK) {
-				// TODO only transform valid models.
-				printDiagnostic(diagnostic, "");
-			}
-			if (eObject instanceof Manager) {
-				generatePrismCode((Manager) eObject);
-			}
+	protected void handleEObject(EObject eObject) throws FileNotFoundException, IOException, CoreException {
+		if (eObject instanceof Manager) {
+			generatePrismCode((Manager) eObject);
 		}
-
 	}
 
 	private void generatePrismCode(Manager manager) throws FileNotFoundException, IOException, CoreException {
@@ -109,12 +67,5 @@ public class Hom2Prism {
 		return s.getName();
 	}
 
-	protected static void printDiagnostic(Diagnostic diagnostic, String indent) {
-		System.out.print(indent);
-		System.out.println(diagnostic.getMessage());
-		for (Diagnostic child : diagnostic.getChildren()) {
-			printDiagnostic(child, indent + "  ");
-		}
-	}
 
 }
